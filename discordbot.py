504edf632484cc55b6203da505cfde1162ccfcb3
takasyun <85444804+takasyun@users.noreply.github.com>
2021-06-07 15:40:39 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import os
 import traceback
 import time
-#import requests
+import requests
 import json
 import copy
 from datetime import datetime, timedelta, timezone
